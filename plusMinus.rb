8db4630df7b6814771459687b2054747c60b8a41
Ali Berenjian <Ali.rberenjian@gmail.com>
2018-03-31 18:56:36 -0700

    Slove PlusMinus

--- plusMinus.rb
+++ plusMinus.rb
@@ -7,15 +7,30 @@ def plusMinus(arr)
     zeros = arr.select{|x| x == 0}
     neg = arr.select{|x| x < 0}
     if pos.length > 0
-        result.push((pos.length.to_f/arr.length).round(arr.length))
+        numtoString = (pos.length.to_f/arr.length).round(arr.length).to_s
+        result.push(check_percsion(numtoString,arr.length))
     end
     if neg.length > 0 
-        result.push((neg.length.to_f/arr.length).round(arr.length))
+        numtoString = (neg.length.to_f/arr.length).round(arr.length).to_s
+        result.push(check_percsion(numtoString,arr.length))
     end
     if zeros.length > 0 
-        result.push((zeros.length.to_f/arr.length).round(arr.length))
+        numtoString = (zeros.length.to_f/arr.length).round(arr.length).to_s
+        result.push(check_percsion(numtoString,arr.length))
     end
     result
+end
+    
+def check_percsion(numtoString,length)
+        indexof = numtoString.index('.')
+        a = numtoString[indexof+1..-1]
+        if (a.length < length)
+            diff = length - a.length
+            diff.times do |n|
+               numtoString += '0' 
+            end
+        end
+    return numtoString
 end
 
 
